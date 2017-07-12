9f29c819a4860a185ec41e8d4dfac90d78a50f6f
carlos <carlos.vargas@simonmediagroup.com>
2017-07-12 10:49:22 -0400

    added video background and adjusted footer 07/11/17

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,37 +1,129 @@
 <!-- Footer -->
 <footer>
-  <section id="footer">
-    <div class="navbar-fixed-bottom">
+  <section id="footer">  
+    <div class="footer navbar-default">
       <div class="container">
+      
       <div class="row">
         <div class="col-sm-12">
-          <ul class="text-center list-unstyled list-inline">
-            <li><a href="#" target="_blank">FB</a></li>
-            <li><a href="#" target="_blank">IG</a></li>
-            <li><a href="#" target="_blank">TW</a></li>
-            <li><a href="#" target="_blank">LK</a></li>
+          <ul class="text-center list-unstyled list-inline social-icons">
+            <li><a href="https://www.facebook.com/thrivevb/" target="_blank"><img class="img-responsive" src="/assets/images/social-icons/fb-circle.svg" alt="Thrive Proactive Health Facebook"></a></li>
+            <li><a href="https://www.instagram.com/thrivevb/" target="_blank"><img class="img-responsive" src="/assets/images/social-icons/insta-circle.svg" alt="Thrive Proactive Health Instagram"></a></li>
+            <li><a href="https://twitter.com/thrive757" target="_blank"><img class="img-responsive" src="/assets/images/social-icons/twitter-circle.svg" alt="Thrive Proactive Health Twitter"></a></li>
+            <li><a href="https://www.linkedin.com/company/thrive-proactive-health" target="_blank"><img class="img-responsive" src="/assets/images/social-icons/linkin-circle.svg" alt="Thrive Proactive Health LinkedIn"></a></li>
           </ul>
         </div>
       </div>
+      
+      <div class="row">
+      	<div class="col-md-4">
+      		<h3>Location</h3>
+      		<p>2830 Virginia Beach Blvd.
+      		<br>Virginia Beach, VA 23452
+      		<br>Located in MarketSquare Shopping Center with OneLife Fitness and Hobby Lobby.</p>
+      	</div>
+      	<div class="col-md-4">
+      		<h3>Hours</h3>
+      		<p>M - 9am to 7pm
+      		<br>Tu - 7:30am to 7pm
+      		<br>W - 7:30am to 7pm
+      		<br>Th - 9am to 7pm
+      		<br>F - 7:30am to 6pm
+      		<br>Sat - 9am to 2pm
+      		<br>Sun - closed</p>
+      	</div>
+      	<div class="col-md-4">
+      		<h3>Contact Us</h3>
+      		
+			<!-- Footer -->
+      		<form id="form12" name="form12" class="wufoo topLabel page" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate action="https://s1mon.wufoo.com/forms/z106zgp6079yqha/#public">
+  
+			<div class="row">
+				<div id="foli1" class="notranslate       form-group col-sm-6">
+					<label class="desc" id="title1" for="Field1">
+					Name<span id="req_1" class="req">*</span>
+					</label>
+
+					<div>
+						<input id="Field1" name="Field1" type="text" class="field text small form-control" value="" maxlength="255" tabindex="1" onkeyup=""       required placeholder="Enter Name" />
+					</div>
+				</div>
+				<div id="foli2" class="notranslate       form-group col-sm-6">
+					<label class="desc" id="title2" for="Field2">
+					Email<span id="req_2" class="req">*</span>
+					</label>
+
+					<div>
+						<input id="Field2" name="Field2" type="email" spellcheck="false" class="field text small form-control" value="" maxlength="255" tabindex="2"       placeholder="Enter Email" required />
+					</div>
+				</div>
+			</div>
+				<div id="foli3" class="notranslate       form-group">
+					<label class="desc" id="title3" for="Field3">
+					Message<span id="req_3" class="req">*</span>
+					</label>
+
+					<div>
+						<textarea id="Field3" name="Field3" class="field textarea small form-control" spellcheck="true" rows="3" cols="50" tabindex="3" onkeyup="" placeholder="Leave a Message" required  ></textarea>
+					</div>
+				</div>
+				<div class="buttons ">
+					<div>
+						<input id="saveForm" name="saveForm" class="btTxt submit btn btn-default" type="submit" value="Submit"
+					 />
+					</div>
+				</div>
+
+				<div class="hide">
+				<label for="comment">Do Not Fill This Out</label>
+				<textarea name="comment" id="comment" rows="1" cols="1"></textarea>
+				<input type="hidden" id="idstamp" name="idstamp" value="O05QiPtVjv/EvjW23enGRvplRZlXs016glWHInU7uQY=" />
+				</div>
+		</form>
+      	</div>
+      </div>
+      
       <div class="row">
         <div class="col-sm-12">
-          <ul class="text-center small">
-            <li class="text-muted">&copy;Thrive Proactive Health</li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms &amp; Conditions</a></li>
+          <ul class="text-center list-unstyled list-inline small">
+            <li class="pull-left"><?php auto_copyright();?> Thrive Proactive Health</li>
+            <li class="pull-right"><a href="#">Privacy Policy</a></li>
+            <li class="pull-right"><a href="#">Terms &amp; Conditions</a></li>
           </ul>
         </div>
       </div>
+      
     </div>
     </div>
   </section>
 </footer>
 
 <!--<script src="/assets/js/main.js"></script>-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
+<script src="/assets/js/wufoo.js"></script>
 
 <!-- Typekit Fonts -->
 <script src="https://use.typekit.net/rxa6uum.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+<?php 
+// No argument required for current year.
+// Otherwise, pass start year as a 4-digit value.
+function auto_copyright($startYear = null) {
+	$thisYear = date('Y');  // get this year as 4-digit value
+    if (!is_numeric($startYear)) {
+		$year = $thisYear; // use this year as default
+	} else {
+		$year = intval($startYear);
+	}
+	if ($year == $thisYear || $year > $thisYear) { // $year cannot be greater than this year - if it is then echo only current year
+		echo "&copy;$thisYear"; // display single year
+	} else {
+		echo "&copy; $year&ndash;$thisYear"; // display range of years
+	}   
+ } 
+?>
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID -->
 <script>
--- index.php
+++ index.php
@@ -10,6 +10,17 @@
 <!--Site's Header-->
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/assets/includes/header.php'); ?>
 
+<div class="video-container">
+  <iframe class="video" src="https://player.vimeo.com/video/198518793?loop=1&background=1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+  <div class="overlay"></div>
+</div>
+
+<img src="/assets/images/text-logo-wht.svg" alt="Thrive Proactive Health">
+
+<div class="about">
+	<p>We are the region’s premier health and wellness clinic. We offer natural and innovative solutions to restore, revive, and optimize the mind &amp; body. We are a team of trained professionals and innovators, and we choose to be a different kind of healthcare facility.</p>
+</div>
+
 
 <!--Site's Footer-->
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/assets/includes/footer.php'); ?>
